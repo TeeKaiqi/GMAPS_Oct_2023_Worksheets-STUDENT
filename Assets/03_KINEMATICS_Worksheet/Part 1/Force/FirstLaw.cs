b21832f590cdb38639d891337827dc0f6ed76ab8
TeeKaiqi <105273769+TeeKaiqi@users.noreply.github.com>
2023-11-20 17:03:53 +0800

    Physics Worksheet
    Finished game imp 1-3

--- Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
@@ -1,21 +1,23 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class FirstLaw : MonoBehaviour
-// {
-//     public Vector3 force;
-//     Rigidbody rb;
+public class FirstLaw : MonoBehaviour
+{
+    public Vector3 force;
+    Rigidbody rb;
 
-//     void Start()
-//     {
-//         rb = // your code here;
-//         rb.// your code here;
-//     }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(force, ForceMode.Impulse);
+     }
 
-//     void FixedUpdate()
-//     {
-//         Debug.Log(transform.position);
-//     }
-// }
+    void FixedUpdate()
+    {
+        Debug.Log(transform.position);
+    }
+}
+
+//This follows Newton's first law because after the initial push, the object kept moving at a consistent speed.
 
