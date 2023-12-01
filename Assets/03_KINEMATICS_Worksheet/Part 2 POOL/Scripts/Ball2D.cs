1de3a88a77b3714d615cc2ed33e8a6670c53045d
TeeKaiqi <105273769+TeeKaiqi@users.noreply.github.com>
2023-12-01 15:04:35 +0800

    fixed extra test matrix
    working on pool problem

--- Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Ball2D.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Ball2D.cs
@@ -22,9 +22,17 @@ public class Ball2D : MonoBehaviour
         Radius = local_sprite_size.x / 2f;
     }
 
+    public class Util
+    {
+        public static float FindDistance(HVector2D p1, HVector2D p2)
+        {
+            //return Mathf.Sqrt(Mathf.Pow(p2.x-p1.x,2)+Mathf.Pow(p2.y-p1.y,2));
+            return Mathf.Sqrt((p2.x - p1.x * p2.x - p1.x) + (p2.y - p1.y * p2.y - p1.y));
+        }
+    }
     public bool IsCollidingWith(float x, float y)
     {
-        float distance = /*your code here*/;
+        float distance = Util.FindDistance(new HVector2D(transform.position),new HVector2D(x,y));
         return distance <= Radius;
     }
 
@@ -41,11 +49,11 @@ public class Ball2D : MonoBehaviour
 
     private void UpdateBall2DPhysics(float deltaTime)
     {
-        float displacementX = /*your code here*/;
-        float displacementY = /*your code here*/;
+        //float displacementX = /*your code here*/;
+        //float displacementY = /*your code here*/;
 
-        Position.x += /*your code here*/;
-        Position.y += /*your code here*/;
+        //Position.x += /*your code here*/;
+        //Position.y += /*your code here*/;
 
         transform.position = new Vector2(/*your code here*/);
     }
